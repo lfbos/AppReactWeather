6b4a221be0cbc555eeaa744278c4665e1405bfbe
Luis Boscan <luife642@gmail.com>
2016-10-26 21:33:20 -0400

    ErrorModal component created

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -3,17 +3,18 @@ var axios = require('axios');
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=28d5500ce9ee2392f11a922f586aa2a0';
 
 module.exports = {
- getTemp: function (location) {
-  var encodedLocation = encodeURIComponent(location);
-  var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
-  return axios.get(requestUrl).then(function(res) {
-   if(res.data.cod && res.data.message) {
-    throw new Error(res.data.message);
-   } else {
-    return res.data.main.temp;
-   }
-  }, function (res) {
-   throw new Error(res.data.message);
-  });
- }
+	getTemp: function(location) {
+		var encodedLocation = encodeURIComponent(location);
+		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+		return axios.get(requestUrl).then(function(res) {
+			if (res.data.cod && res.data.message) {
+				throw new Error(res.data.message);
+			} else {
+				return res.data.main.temp;
+			}
+		}, function(res) {
+			throw new Error(res.data.message);
+		});
+	}
 }
