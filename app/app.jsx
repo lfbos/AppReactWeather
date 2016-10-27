0334f765696a64c53ae6f8e85377d494ba1f24d6
Luis Boscan <luife642@gmail.com>
2016-10-26 21:43:24 -0400

    Custom styles added

--- app/app.jsx
+++ app/app.jsx
@@ -9,6 +9,10 @@ var Examples = require('Examples');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+// App css
+require('style!css!applicationStyles')
 
 var app = document.getElementById('app');
 
